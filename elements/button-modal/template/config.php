f72da5e37ca10ed8197608154e7d40720d7aace5
Md. Anam Hossain <anam@themexpert.com>
2019-09-03 23:52:16 +0600

    button modal and post carousel addon resource dependency updated

--- elements/button-modal/template/config.php
+++ elements/button-modal/template/config.php
@@ -34,11 +34,21 @@ class wkfe_modal_button extends Widget_Base {
 	}
 
 	/**
+	 * A list of style that the widgets is depended in
+	 **/
+	public function get_style_depends() {
+        return [
+            'widgetkit_bs',
+            'widgetkit_main',
+        ];
+    }
+	/**
 	 * A list of scripts that the widgets is depended in
-	 * @since 1.3.0
 	 **/
 	public function get_script_depends() {
-		return [ 'widgetkit-for-elementor-button' ];
+		return [ 
+			'widgetkit-main',
+		 ];
 	}
 
 	protected function _register_controls() {
--- elements/carousel/template/config.php
+++ elements/carousel/template/config.php
@@ -36,11 +36,23 @@ class wkfe_carousel extends Widget_Base {
 	}
 
 	/**
+	 * A list of style that the widgets is depended in
+	 **/
+	public function get_style_depends() {
+        return [
+            'widgetkit_bs',
+            'owl-css',
+            'widgetkit_main',
+        ];
+    }
+	/**
 	 * A list of scripts that the widgets is depended in
-	 * @since 1.3.0
 	 **/
 	public function get_script_depends() {
-		return [ 'owl-carousel' ];
+		return [ 
+			'owl-carousel',
+			'widgetkit-main',
+		 ];
 	}
 
 	protected function _register_controls() {
