2b1cbbbf629e420ccd3b634e487efc185e4b113f
Md. Anam Hossain <anam@themexpert.com>
2019-11-07 13:44:25 +0600

    pro button padding fixed in dashboard

--- admin/admin-init.php
+++ admin/admin-init.php
@@ -707,12 +707,12 @@ class Widgetkit_Admin
                     </div>
                     
                     <?php if (!apply_filters('wkpro_enabled', false)) :?>
-                    <div class="wk-width-1-4">
+                    <div class="wk-width-1-4 pro-sidebar">
                         <div wk-sticky="offset: 40">
                             <div class="wk-card wk-card-default wk-card-body  wk-background-small wk-text-center">
                                 <img class="wk-margin-small-top" src="<?php echo plugins_url('/assets/images/widgetkit-pro.svg', __FILE__)?>" width="150" wk-svg>
                                 <p class="wk-text-muted">Get the pro version of <strong>WidgetKit</strong> for more stunning elements and customization options.</p>
-                                <a href="https://themesgrove.com/widgetkit-for-elementor/?utm_campaign=widgetkit-pro&utm_medium=wp-admin&utm_source=pro-feature-button" target="_blank" class="wk-button wk-button-primary "><span class="wk-icon wk-margin-small-right" wk-icon="unlock"></span>Upgrade to Pro</a>
+                                <a href="https://themesgrove.com/widgetkit-for-elementor/?utm_campaign=widgetkit-pro&utm_medium=wp-admin&utm_source=pro-feature-button" target="_blank" class="wk-button wk-button-primary wk-padding-remove-vertical wk-padding-small"><span class="wk-icon wk-margin-small-right" wk-icon="unlock"></span>Upgrade to Pro</a>
                             </div>
                         </div>
                     </div>
