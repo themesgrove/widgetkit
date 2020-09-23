20323429ea33a8c7b7b1f73a9688d06cdcceb6c7
Md. Anam Hossain <anam@themexpert.com>
2020-09-23 16:38:33 +0600

    conditional text added for license tab in dashboard

--- admin/admin-init.php
+++ admin/admin-init.php
@@ -1439,17 +1439,29 @@ class Widgetkit_Admin
                                         <li class="wk-pro-license">
                                             <div class="wk-padding-remove">
 
-                                                <h2>Activate Your License</h2>
+                                                <h2>
+                                                    <?php 
+                                                        get_option("wk_pro_license_key") 
+                                                            ? esc_html_e("Deactivate", "widgetkit-for-elementor") 
+                                                            : esc_html_e("Activate", "widgetkit-for-elementor"); 
+                                                        esc_html_e(" Your License", "widgetkit-for-elementor");
+                                                    ?> 
+                                                </h2>
                                                 
-                                                <div class="wk-padding-small wk-background-muted">
+                                                <div class="wk-padding-small wk-background-muted wk-license-card">
                                                     <div class="wk-card">
-                                                        <div class="wk-card-header"><?php echo esc_html__('Enter your license key here, to activate WPOnepager Pro, and get feature updates, premium support and unlimited access to the template library.', 'widgetkit-for-elementor');?></div>
+                                                        <?php if(! get_option("wk_pro_license_key")): ?>
+                                                        <div class="wk-card-header"><?php echo esc_html__('Enter your license key here, to activate Widgetkit Pro, and get feature updates, premium support and unlimited access to the template library.', 'widgetkit-for-elementor');?></div>
+                                                        <?php endif; ?>
+
                                                         <div class="wk-card-body">
+                                                            <?php if(! get_option("wk_pro_license_key")): ?>
                                                             <ol>
                                                                 <li> <?php echo esc_html__('Log in to your account to get your license key.', 'widgetkit-for-elementor') ?> </li>
-                                                                <li> <?php echo esc_html__('If you don\'t yet have a license key, get WPOnepager Pro now.', 'widgetkit-for-elementor') ?> </li>
+                                                                <li> <?php echo esc_html__('If you don\'t yet have a license key, get Widgetkit Pro now.', 'widgetkit-for-elementor') ?> </li>
                                                                 <li> <?php echo esc_html__('Copy the license key from your account and paste it below.', 'widgetkit-for-elementor') ?> </li>
                                                             </ol>
+                                                            <?php endif; ?>
                                                             <div class="license-checker-wrapper">
                                                                 <?php //wp_nonce_field( 'wk-pro-license' ); ?>
                                                                 <input class="wk-input" type="text" name="license-input" id="license-input" value="<?php echo get_option('wk_pro_license_key') ?: ''; ?>">
@@ -1459,10 +1471,13 @@ class Widgetkit_Admin
                                                                     <button class="activate-license wk-button wk-button-primary">Activate</button>
                                                                 <?php endif;?>  
                                                             </div>
+                                                            <?php if(! get_option("wk_pro_license_key")): ?>
                                                             <div class="demo-text">
                                                                 <?php echo esc_html__('Your license key should look something like this: ', 'widgetkit-for-elementor');?>
                                                                 <code>fb351f05958872E193feb37a505a84be</code>
                                                             </div>
+                                                            <?php endif; ?>
+
                                                             <div class="response"></div>
                                                         </div>
                                                     </div>
