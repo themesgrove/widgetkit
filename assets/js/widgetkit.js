07492c3e46b5deb9c1275b2cea7311b9344f7ee2
Amir Hossain <a93hossain@gmail.com>
2022-12-25 12:32:43 +0600

    Fix the active class issue of the Advanced tab element

--- assets/js/widgetkit.js
+++ assets/js/widgetkit.js
@@ -237,18 +237,38 @@ jQuery( window ).on( 'elementor/frontend/init', function() {
 (function($) {
     "use strict";
 	var AdvancedTabHandler = function () {
-        $('.wk-adv-tab-wrapper .wk-adv-tabs-nav li:first-child').addClass('active');
-		$('.wk-adv-tabs-nav li a').on('click',function() {
-			// Check for active
-            $('.wk-adv-tabs-nav li').removeClass('active');
-			$(this).parent().addClass('active');
+        $('.wk-adv-tab-wrapper .wk-adv-tabs-nav li a:first-child').addClass('active');
+        $('.wk-adv-tabs-nav li a').on('click',function(e) {
+            e.preventDefault();
+            let $parent = $(e.target).parents(".wk-adv-tab-wrapper");
+            $parent.find('a').each((index, element) => {
+          	    $(element).removeClass('active');
+            })
+            $(e.target).closest('a').addClass('active');
 
 			// Display active tab
-			let currentTab = $(this).attr('href');
-			$('.wk-tabs-content-wrap .wk-tabs-content').hide();
+			let currentTab = $(e.target).closest('a').attr('href');
+            $parent.find('.wk-tabs-content').hide();
 			$(currentTab).show();
 			return false;
+
 		});
+
+
+
+
+
+		// $('.wk-adv-tabs-nav li a').on('click',function() {
+		// 	// Check for active
+        //     $('.wk-adv-tabs-nav li').removeClass('active');
+		// 	$(this).parent().addClass('active');
+
+		// 	// Display active tab
+		// 	let currentTab = $(this).attr('href');
+		// 	$('.wk-tabs-content-wrap .wk-tabs-content').hide();
+		// 	$(currentTab).show();
+		// 	return false;
+		// });
 	}
 
 	// Run this code under Elementor.
