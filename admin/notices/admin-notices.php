c164089066ba837f06e79b62fee5f3f2d6d5769b
SagorIslamOfficial <alaminislam1274@gmail.com>
2025-04-23 15:19:55 +0600

    [Fix] Add nonce to dismiss notice URL

--- admin/notices/admin-notices.php
+++ admin/notices/admin-notices.php
@@ -6,17 +6,25 @@ add_action('admin_init','wk_td_ads_dismiss_notice');
 
 function wk_td_admin_ads()
 {
-	if (get_option("wk-td-ads-notice")) {
-		return;
-	}
+    if (get_option("wk-td-ads-notice")) {
+        return;
+    }
+    
+    // Create dismiss URL with nonce
+    $dismiss_url = add_query_arg(array(
+        'dismissed' => '1',
+        'nonce' => wp_create_nonce('wk_td_dismiss_notice')
+    ), admin_url('admin.php'));
 ?>
+
 <div class="wk-td-ads-notice notice notice-success is-dismissible" style="padding: 30px 30px 20px">
     <img style="max-width:200px"
         src="<?php echo esc_attr(plugin_dir_url(__FILE__) . '../assets/images/thrivedesk-logo.png'); ?>">
     <p style="font-size:16px">
         <?php esc_html_e('Your customers deserve better customer support and You deserve the peace of mind.', 'widgetkit-for-elementor');?>
-		 <a href="<?php echo esc_url('https://www.thrivedesk.com');?>"><strong>Try ThriveDesk</strong></a>
+		<a href="<?php echo esc_url('https://www.thrivedesk.com');?>"><strong>Try ThriveDesk</strong></a>
     </p>
+    <a href="<?php echo esc_url($dismiss_url); ?>" class="notice-dismiss"><span class="screen-reader-text">Dismiss this notice.</span></a>
 </div>
 <?php
 }
