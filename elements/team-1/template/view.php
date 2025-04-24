fb57949891b01432125f469dc0ce6947820ee718
SagorIslamOfficial <alaminislam1274@gmail.com>
2025-04-24 11:58:56 +0600

    [Refactor] Replace image output with `wp_get_attachment_image` and fallback handling

--- elements/team-1/template/view.php
+++ elements/team-1/template/view.php
@@ -9,7 +9,12 @@
             <!-- First Team Block -->
             <div class="team-each-wrap">
                 <div class="team-block">
-                    <img src="<?php echo esc_url($settings['team_image']['url']);?>" alt="<?php echo esc_attr($settings['team_name']);?>">   
+                    <?php if (!empty($settings['team_image']['id'])) {
+                        echo wp_get_attachment_image($settings['team_image']['id'], 'full', false, ['alt' => esc_attr($settings['team_name'])]);
+                    } elseif (!empty($settings['team_image']['url'])) {
+                        // phpcs:ignore PluginCheck.CodeAnalysis.ImageFunctions.NonEnqueuedImage
+                        echo '<img src="' . esc_url($settings['team_image']['url']) . '" alt="' . esc_attr($settings['team_name']) . '">';
+                    } ?>   
                     <?php if ( ! empty( $settings['content'] ) ) : ?>
                         <div class="team-content">
                             <p><?php echo esc_html($settings['content']); ?></p>
