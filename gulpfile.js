65fb11216ed90c8f78a731858f985f0fb68f4b4e
ahossain9 <a93hossain@gmail.com>
2025-04-29 11:29:19 +0600

    Make it compatiable with Nodejs latest version

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 var less = require('gulp-less');
 var shell = require("shelljs");
 // var path = require('path');
